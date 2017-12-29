8674df2f2f925a9aedf73cb47824c677d46df64b
Javier González Rojo <>
2017-12-29 18:03:48 +0100

    Añadido el uso de middlewares, redux-thunk para la ejecución de código asíncrono y refactorizadas las actions para seguir la filosofía de los action creators;9C

--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -1,21 +1,37 @@
-import * as actionTypes from '../actions';
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.id);
+    return updateObject(state, {results: updatedArray});
+};
+
 const reducer = (state = initialState, action) => {
+    console.log('action', action);
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-        return {
-            ...state,
-            results: state.results.concat({id: new Date(), value: action.result})
-        }
-    case actionTypes.DELETE_RESULT:
-        return {
-            ...state,
-            results: state.results.filter(result => result.id !== action.id)
-        }
+            /*
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: action.result})
+            }
+            */
+            // sustituimos lo anterior por una llamada a la nueva función de utility
+            return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) });
+        case actionTypes.DELETE_RESULT:
+            /*
+            return {
+                ...state,
+                results: state.results.filter(result => result.id !== action.id)
+            }
+            */
+            // sustituimos lo anterior por una llamada a la nueva función deleteReault
+            // así todas las llamadas del switch serán muy simples
+            return deleteResult(state, action);
         default:
             return state;
     }
